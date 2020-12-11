325449746b9e527d264cb80e0c87366416cc16a8
GowthamanM <gowthamancode@gmail.com>
2020-12-11 13:19:13 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,10 +1,17 @@
 const puppeteer = require('puppeteer');
 
-puppeteer.launch().then(async browser => {
-  const page = await browser.newPage();
-  page.goto("http://localhost:4200");
-  page
-    .waitForSelector('#result')
-    .then(() => console.log('got it'));
-    browser.close();
-});
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.goto('http://localhost:4200/', { waitUntil: 'networkidle0' }) // check networkidle0 parameter and others here: https://pptr.dev/#?product=Puppeteer&version=v2.1.1&show=api-pagegotourl-options
+  const pageClicked = await page.evaluate(() => {
+    return !!document.querySelector('body > app-root > app-header > div > div > div.submit > button') // !! converts anything to boolean
+  })
+  if (pageClicked) { // you had the condition reversed. Not sure if it was intended.
+    console.log('True')
+  } else {
+    console.log('False')
+  }
+  await browser.close()
+})()
