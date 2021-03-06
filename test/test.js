c3d8141e70d30d26c3fe066fafd7f3c2b1a8a27d
GowthamanM-Neo <gowthaman@iamneo.ai>
2021-03-06 17:00:17 +0530

    header changed

--- test/test.js
+++ test/test.js
@@ -10,6 +10,63 @@ const checkIfClassExists = (str = '') => {
   const page = await browser.newPage()
 
   await page.goto('http://localhost:4200/', { waitUntil: 'networkidle0' }) // check networkidle0 parameter and others here: https://pptr.dev/#?product=Puppeteer&version=v2.1.1&show=api-pagegotourl-options
-  console.log(checkIfClassExists("submit"));
+
+  try {
+    await page.waitForSelector('#heading', { timeout: 1000 });
+    let element = await page.$('#heading');
+    let value = await page.evaluate(el => el.textContent, element);
+    if (value === "Temperature Converter") {
+      console.log('TESTCASE:test_case1:success');
+    }else{
+      console.log('TESTCASE:test_case1:failure');
+    }
+  }catch{
+    console.log('TESTCASE:test_case1:failure');
+  }
+
   await browser.close()
-})()
+})();
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.goto('http://localhost:4200/', { waitUntil: 'networkidle0' }) // check networkidle0 parameter and others here: https://pptr.dev/#?product=Puppeteer&version=v2.1.1&show=api-pagegotourl-options
+
+  try {
+    await page.waitForSelector('#centigradeButton', { timeout: 1000 });
+    let element = await page.$('#centigradeButton');
+    let value = await page.evaluate(el => el.textContent, element);
+    if (value === "Convert to Centigrade") {
+      console.log('TESTCASE:test_case2:success');
+    }else{
+      console.log('TESTCASE:test_case2:failure');
+    }
+  }catch{
+    console.log('TESTCASE:test_case2:failure');
+  }
+
+  await browser.close()
+})();
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.goto('http://localhost:4200/', { waitUntil: 'networkidle0' }) // check networkidle0 parameter and others here: https://pptr.dev/#?product=Puppeteer&version=v2.1.1&show=api-pagegotourl-options
+
+  try {
+    await page.waitForSelector('#fahrenheitButton', { timeout: 1000 });
+    let element = await page.$('#fahrenheitButton');
+    let value = await page.evaluate(el => el.textContent, element);
+    if (value === "Covert to Fahrenheit") {
+      console.log('TESTCASE:test_case3:success');
+    }else{
+      console.log('TESTCASE:test_case3:failure');
+    }
+  }catch{
+    console.log('TESTCASE:test_case3:failure');
+  }
+
+  await browser.close()
+})();
